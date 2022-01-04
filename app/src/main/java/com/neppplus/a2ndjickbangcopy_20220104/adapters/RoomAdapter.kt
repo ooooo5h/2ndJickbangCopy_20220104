d6a6be0a31f2cf572592899af78c4b9de604d6f8
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 17:33:06 +0900

    RoomAdapter 클래스 제작 중

---
+++ app/src/main/java/com/neppplus/a2ndjickbangcopy_20220104/adapters/RoomAdapter.kt
@@ -0,0 +1,25 @@
+package com.neppplus.a2ndjickbangcopy_20220104.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RoomAdapter : RecyclerView.Adapter<RoomAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(row : View) : RecyclerView.ViewHolder(row) {
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+}

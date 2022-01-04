ccd71cb9ed5e7ace7add46b241c3192461efb849
ooooo5h <ooooo5h91@gmail.com>
2022-01-04 18:05:45 +0900

    다시 RoomData로 만들기

--- app/src/main/java/com/neppplus/a2ndjickbangcopy_20220104/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/a2ndjickbangcopy_20220104/ViewRoomDetailActivity.kt
@@ -2,10 +2,14 @@ package com.neppplus.a2ndjickbangcopy_20220104
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.a2ndjickbangcopy_20220104.models.RoomData
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+//        받아온 SerializableExtra 를 RoomData로 다시 원복
+        val roomData = intent.getSerializableExtra("room") as RoomData
     }
 }
